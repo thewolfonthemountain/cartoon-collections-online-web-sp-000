a7edebfa16a3812f70bbc3362ab660477fb6b29f
Sean Langton <slangton1@gmail.com>
2018-10-30 01:14:55 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(array)
   array.any?{|name| name.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?{|snack| cheese_types.include?(snack)}
 end
