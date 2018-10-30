ea8e5f0e48d65c68ff58eb3ea64874b097f5d964
Sean Langton <slangton1@gmail.com>
2018-10-30 01:23:43 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,8 +12,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  done = ""
-  array.each {|cheese| done = cheese if cheese_types.include?|cheese|}
-
+  array.find {|cheese| cheese_types.include?(cheese)}
   end
 end
